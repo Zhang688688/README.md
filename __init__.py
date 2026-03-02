78b28aa3bcac4885b2f136ff3bd4324d171f9c4c
GUANGRUI ZHANG <johnzhang688688@gmail.com>
2026-03-02 10:32:14 +0800

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,5 @@
+# __init__.py - Import application module
+import application
+
+# Optional: Import specific functions/classes from the application module
+from application import emotion_detector, load_emotion_model
